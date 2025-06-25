e396ad755d3efd34fddee5be78501102204cd8fb
rashmi-ranjan-sahoo-dev <rinkuasahoo04@gmail.com>
2025-06-25 11:43:38 +0530

    second brain start

---
+++ week15/brainly/backend/src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken'
+
+const app = express();
+
+app.post('/api/v1/signup',function(req,res){
+
+})
+
+app.post('/api/v1/signin',function(req,res){
+    
+})
+
+app.post('/api/v1/content',function(req,res){
+
+})
+
+app.get('/api/v1/content',function(req,res){
+    
+})
+
+app.delete('/api/v1/content',function(req,res){
+    
+})
+
+app.get('/api/v1/brain/"shareLink',function(req,res){
+    
+})
+
+

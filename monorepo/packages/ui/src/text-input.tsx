7b66e41c644e851da99c9a454aabda28b37c82f0
rashmi-ranjan-sahoo-dev <rinkuasahoo04@gmail.com>
2025-08-07 16:44:29 +0530

    ok

---
+++ monorepo/packages/ui/src/text-input.tsx
@@ -0,0 +1,18 @@
+interface PropType {
+    placeholder: string;
+    size: "big" | "small"
+}
+
+
+
+export function TextInput({
+    placeholder,size
+}: PropType){
+   return <input placeholder={placeholder}
+   style={{
+    padding: size === "big" ?20: 10,
+    margin: size === "big" ?20: 10,
+    background: "black",
+    borderWidth: 1
+   }}></input> 
+}
